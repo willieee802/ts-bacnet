689c82afd305cd384a585c5d06f29e5916a54f5f
Taner <44664524+tanerqy@users.noreply.github.com>
2023-09-07 14:34:30 +0200

    working package

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as enums from "./src/enum";
-import * as client from "./src/client";
+import client from "./src/client";
 import _ from "underscore";
 
 export { enums, _ };
+
 export default client;
